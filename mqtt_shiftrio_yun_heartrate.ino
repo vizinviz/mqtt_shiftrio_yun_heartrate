4991e3a7b9892423b40988887e0cc4d9cb8e80e8
chrischne <chrischne@gmail.com>
2019-10-20 18:31:13 +0200

    calculate heart rate

--- mqtt_shiftrio_yun_heartrate.ino
+++ mqtt_shiftrio_yun_heartrate.ino
@@ -6,9 +6,14 @@ BridgeClient net;
 MQTTClient client;
 
 unsigned long lastMillis = 0;
-float theInterrupt = 0;
+long currentInterrupt = millis();
+long lastInterrupt = millis();
 int prevCount = 0;
 int counter = 0;
+
+unsigned long temp[5];
+int index = 0;
+
 
 void connect() {
   while (!client.connect("arduino-yun-vizinviz", "ecf929fe", "f65bde19d8e46d67")) {
@@ -23,6 +28,7 @@ void setup() {
   client.begin("broker.shiftr.io", net);
   connect();
 
+  arrayInit();
   attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);//set interrupt 0,digital port 3
 }
 
@@ -36,11 +42,18 @@ void loop() {
   // publish a message roughly every second.
   if (prevCount!=counter) {
     prevCount = counter;
+
+    float sum = 0;
+    for(int i=0; i<5; i++){
+      sum+=temp[i];
+    }
+    float avg = sum/5.0;
+    float rate = 60000/avg;
     //client.publish("/hello", "world");
     //client.publish("/velo", "moto");
-    String topic = "/interrupt";
+    String topic = "/rate";
     String payload = "";
-    payload += counter;
+    payload += rate;
     digitalWrite(LED_BUILTIN, HIGH);
     client.publish(topic, payload);
 
@@ -56,7 +69,23 @@ void loop() {
 void interrupt()
 {
 
-  theInterrupt = millis();
+  lastInterrupt = currentInterrupt;
+  currentInterrupt = millis();
+  long diff = currentInterrupt - lastInterrupt;
+  temp[index] = diff;
+  index++;
+  if(index>=5){
+    index = 0;
+  }
   counter++;
+}
+
+/*Function: Initialization for the array(temp)*/
+void arrayInit()
+{
+  for (int i = 0; i < 5; i++)
+  {
+    temp[i] = 0;
+  }
 
 }
